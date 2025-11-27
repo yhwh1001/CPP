53a4ff2332ced83bfac799023a488290cced1a6d
reimanel9999 <158600257+reimanel9999@users.noreply.github.com>
2025-11-27 20:27:49 +0100

    new main

--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: manelcarvalho <manelcarvalho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:54:36 by manelcarval       #+#    #+#             */
-/*   Updated: 2025/11/27 18:29:25 by manelcarval      ###   ########.fr       */
+/*   Updated: 2025/11/27 20:27:23 by manelcarval      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,54 @@
 #include <iostream>
 #include <fstream> 
 
-int	main() {
-	Harl harl;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./harl [DEBUG|INFO|WARNING|ERROR]" << std::endl;
+        return 1;
+    }
     
-    std::cout << "=== Testing all valid levels ===" << std::endl;
-    std::cout << std::endl;
+    Harl harl;
+    harl.complain(argv[1]);
     
-    std::cout << "[DEBUG]" << std::endl;
-    harl.complain("DEBUG");
-    std::cout << std::endl;
-    
-    std::cout << "[INFO]" << std::endl;
-    harl.complain("INFO");
-    std::cout << std::endl;
-    
-    std::cout << "[WARNING]" << std::endl;
-    harl.complain("WARNING");
-    std::cout << std::endl;
-    
-    std::cout << "[ERROR]" << std::endl;
-    harl.complain("ERROR");
-    std::cout << std::endl;
-    
-    std::cout << "=== Testing invalid levels ===" << std::endl;
-    std::cout << std::endl;
-    
-    std::cout << "[INVALID]" << std::endl;
-    harl.complain("INVALID");
-    std::cout << std::endl;
-    
-    std::cout << "[lowercase test]" << std::endl;
-    harl.complain("debug");
-    std::cout << std::endl;
-    
-    std::cout << "[Empty string]" << std::endl;
-    harl.complain("");
-    std::cout << std::endl;
-    
-    // std::cout << "=== Random complaining ===" << std::endl;
-    // std::cout << std::endl;
-    
-    // harl.complain("ERROR");
-    // harl.complain("DEBUG");
-    // harl.complain("WARNING");
-    // harl.complain("INFO");
-	return 0;
+    return 0;
 }
+
+// int	main() {
+// 	Harl harl;
+    
+//     std::cout << "=== Testing all valid levels ===" << std::endl;
+//     std::cout << std::endl;
+    
+//     std::cout << "[DEBUG]" << std::endl;
+//     harl.complain("DEBUG");
+//     std::cout << std::endl;
+    
+//     std::cout << "[INFO]" << std::endl;
+//     harl.complain("INFO");
+//     std::cout << std::endl;
+
+//     std::cout << "[WARNING]" << std::endl;
+//     harl.complain("WARNING");
+//     std::cout << std::endl;
+    
+//     std::cout << "[ERROR]" << std::endl;
+//     harl.complain("ERROR");
+//     std::cout << std::endl;
+    
+//     std::cout << "=== Testing invalid levels ===" << std::endl;
+//     std::cout << std::endl;
+    
+//     std::cout << "[INVALID]" << std::endl;
+//     harl.complain("INVALID");
+//     std::cout << std::endl;
+    
+//     std::cout << "[lowercase test]" << std::endl;
+//     harl.complain("debug");
+//     std::cout << std::endl;
+    
+//     std::cout << "[Empty string]" << std::endl;
+//     harl.complain("");
+//     std::cout << std::endl;
+    
+// 	return 0;
+// }
