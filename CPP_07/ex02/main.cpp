675b12d07001c38627e06b1211bb1421373e4a36
Manuel Carvalho <mcarvalh@c3a1c1.42berlin.de>
2026-02-23 12:30:34 +0100

    lezz goo

--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -4,8 +4,16 @@
 #include "Array.hpp"
 
 #define MAX_VAL 750
+
+void separator(std::string title)
+{
+	std::cout << "------------------------" << std::endl;
+	std::cout << "=== " << title << " ===" << std::endl;
+}
+
 int main(int, char**) 
 {
+	separator("INTs");
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
@@ -18,9 +26,9 @@ int main(int, char**)
     //SCOPE
     {
         Array<int> tmp = numbers;
-        Array<int> test(tmp);
+        Array<int> copy(tmp);
     }
-
+	//if this passes, deep copy works great! 
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
@@ -29,27 +37,51 @@ int main(int, char**)
             return 1;
         }
     }
-    try
-    {
+    try {
         numbers[-2] = 0;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
-    try
-    {
+    try {
         numbers[MAX_VAL] = 0;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
+    delete [] mirror;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
+	separator("Strings");
+	Array<std::string>	alphabet(26);
+	std::string*		strMirror = new std::string[26];
+	for (int m = 0; m < 26; m++){
+		alphabet[m] = (char)('a' + m);
+		strMirror[m] = std::string(1, 'a' + m);
+	}
+	{
+		Array<std::string> tmp = alphabet;
+		Array<std::string> copy(tmp);
+	}
+	for (int m = 0; m < 26; m++) {
+		if (alphabet[m] != strMirror[m])
+			std::cout << "Values don't match... " << alphabet[m] << " unequal to " << strMirror[m] << std::endl;
+		std::cout << "Matching values of " << alphabet[m] << " and " << strMirror[m] << std::endl;
+	}
+	try {
+		alphabet[-3] = "Hello";
+	}
+	catch (const std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		alphabet[23] = "Occult...";
+		alphabet[26] = "WORLD";
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << alphabet[23] << std::endl;
     return 0;
 }
